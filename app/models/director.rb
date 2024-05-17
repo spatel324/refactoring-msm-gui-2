c1c827b5fcf3dbc123f32fb0b1b39f45bb692950
spatel324 <165313084+spatel324@users.noreply.github.com>
2024-05-17 17:30:28 +0000

    final working version

--- app/models/director.rb
+++ app/models/director.rb
@@ -12,12 +12,6 @@
 #
 class Director < ApplicationRecord
   validates(:name, presence: true)
+  has_many(:filmography, class_name: "Movie", foreign_key: "director_id")
 
-  def filmography
-    key = self.id
-
-    the_many = Movie.where({ :director_id => key })
-
-    return the_many
-  end
 end
